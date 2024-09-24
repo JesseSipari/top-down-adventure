f5407e9d78d701b08b250d19a175a1f4ffc9a316
Jesse Sipari <Jesse.Sipari@student.lab.fi>
2024-09-24 18:09:16 +0300

    feat: feat: Add player character animations
    Adds idle and running animations to the player character

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <ios>
 #include <raylib.h>
 #include <vector>
 #include <raymath.h>
@@ -15,10 +16,22 @@ int main()
 		float speed = 4.0f;
 		float scale = 4.0f;
 		Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
+		Texture2D knight_idle = LoadTexture("characters/knight_idle_spritesheet.png");
+		Texture2D knight_run = LoadTexture("characters/knight_run_spritesheet.png");
 		Vector2 knightPosition = {
 			static_cast<float>(windowDimensions[0]) / 2.0f - scale * (0.5f * static_cast<float>(knight.width) / 6.0f),
 			static_cast<float>(windowDimensions[1]) / 2.0f - scale * (0.5f * static_cast<float>(knight.height) / 8.0f)
 		};
+
+
+		// 1 = facing right, -1 = facing left
+		float rightLeft{ 1.f };
+
+		// animation variables
+		float runningTime{};
+		int frame{};
+		constexpr int maxFrames{ 6 };
+		constexpr float updateTime{ 1.f/12.f};
 
 		// Main game loop
 		while (!WindowShouldClose())
@@ -32,23 +45,43 @@ int main()
 			if (IsKeyDown(KEY_W)) direction.y -= 1.0f;
 			if (IsKeyDown(KEY_S)) direction.y += 1.0f;
 			if (Vector2Length(direction) != 0.0f)
+			
+			
 			{
+				// set mapPosition = mapPosition - direction
 				mapPosition = Vector2Subtract(mapPosition, Vector2Scale(Vector2Normalize(direction), speed));
+				direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
+				knight = knight_run;
+			}
+			else
+			{
+				knight = knight_idle;
 			}
 
 			// Draw the map
 			DrawTextureEx (map, mapPosition, 0, scale, WHITE);
 
+			// Update animation frame
+			runningTime += GetFrameTime();
+			if (runningTime >= updateTime)
+			{
+				frame++;
+				runningTime = 0.f;
+				if (frame > maxFrames) frame = 0;
+			}
+
 			// Draw the knight
-			Rectangle source{ 0.f, 0.f, static_cast<float>(knight.width) / 6.f, static_cast<float>(knight.height) };
+			
+			Rectangle source{ frame * static_cast<float>(knight.width) / 6.f, 0.f, rightLeft * static_cast<float>(knight.width) / 6.f, static_cast<float>(knight.height) };
 			Rectangle dest{ knightPosition.x, knightPosition.y, scale * static_cast<float>(knight.width) / 6.f, scale * static_cast<float>(knight.height) };
 			DrawTexturePro(knight, source, dest, Vector2{}, 0.f, WHITE);
 
 			EndDrawing();
 		}
-			
+		
 		UnloadTexture(map);
 		UnloadTexture(knight);
 		CloseWindow();
 
+		return 0;
 }
